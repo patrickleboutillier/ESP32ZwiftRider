014c46fed2d3c030547d42a7446ac1214b504d41
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2022-03-26 16:22:22 -0400

    cleanup

--- ESP32ZwiftRider.ino
+++ ESP32ZwiftRider.ino
@@ -5,14 +5,12 @@
 #include <BLE2902.h>
 
 
-// fitness machine service uuid, as defined in gatt specifications
+// fitness machine service uuid, as defined in GATT specifications
 #define FITNESS_MACHINE_SERVICE   BLEUUID((uint16_t)0x1826) 
 
 // BLE Characteristics required
 BLECharacteristic indoorBike(BLEUUID((uint16_t)0x2AD2), BLECharacteristic::PROPERTY_NOTIFY) ;
 BLECharacteristic fitnessMachineControlPoint(BLEUUID((uint16_t)0x2AD9), BLECharacteristic::PROPERTY_INDICATE | BLECharacteristic::PROPERTY_WRITE);
-BLECharacteristic fitnessMachineStatus(BLEUUID((uint16_t)0x2ADA), BLECharacteristic::PROPERTY_NOTIFY);
-BLECharacteristic fitnessMachineFeature(BLEUUID((uint16_t)0x2ACC), BLECharacteristic::PROPERTY_READ);  
 
 
 #define CADENCE     39
@@ -53,30 +51,17 @@ void InitBLEServer() {
   indoorBike.addDescriptor(new BLE2902()) ;
   pFitness->addCharacteristic(&indoorBike) ;
 
-  BLE2902* descr = new BLE2902();
-  fitnessMachineControlPoint.addDescriptor(descr);
+  fitnessMachineControlPoint.addDescriptor(new BLE2902());
   pFitness->addCharacteristic(&fitnessMachineControlPoint);
-
-  pFitness->addCharacteristic(&fitnessMachineStatus) ;
-  fitnessMachineStatus.addDescriptor(new BLE2902());
-  
-  fitnessMachineFeature.addDescriptor(new BLE2902());
-  pFitness->addCharacteristic(&fitnessMachineFeature);
 
   pFitness->start() ;
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(FITNESS_MACHINE_SERVICE);
-  const std::string fitnessData = { 0b00000001, 0b00100000, 0b00000000 } ;
-  BLEAdvertisementData advertisementData = BLEAdvertisementData() ;
-  advertisementData.setServiceData(FITNESS_MACHINE_SERVICE, fitnessData) ;
-  
-  pAdvertising->setAdvertisementData(advertisementData) ;
   pAdvertising->setScanResponse(true) ;
   pAdvertising->setMinPreferred(0x06) ;
   BLEDevice::startAdvertising() ;
 }
-
 
 
 #define MAX_CADENCE 120
@@ -109,26 +94,6 @@ void sendData(uint16_t power, uint16_t cadence){
   } ;
   indoorBike.setValue(indoorBikeData, 8) ;
   indoorBike.notify() ;
-
-  /*
-  // Pretend we support all the features so we get all the messages.
-  //const uint32_t fitnessMachineFeaturesDef = 0b00000000000000000100000010001010 ; 
-  //const uint32_t targetSettingFeaturesDef =  0b00000000000000010010000000001000 ;                        
-  const uint32_t fitnessMachineFeaturesDef = 0b00000000000000011111111111111111 ; 
-  const uint32_t targetSettingFeaturesDef =  0b00000000000000011111111111111111 ;
-  uint8_t fitnessMachineFeatureData[8] = {  // values for setup - little endian order
-    (uint8_t)(fitnessMachineFeaturesDef & 0xff),
-    (uint8_t)(fitnessMachineFeaturesDef >> 8),
-    (uint8_t)(fitnessMachineFeaturesDef >> 16),
-    (uint8_t)(fitnessMachineFeaturesDef >> 24),
-    (uint8_t)(targetSettingFeaturesDef & 0xff),
-    (uint8_t)(targetSettingFeaturesDef >> 8),
-    (uint8_t)(targetSettingFeaturesDef >> 16),
-    (uint8_t)(targetSettingFeaturesDef >> 24) 
-  } ;
-  fitnessMachineFeature.setValue(fitnessMachineFeatureData, 8) ; // flags
-  fitnessMachineFeature.notify() ;
-  */
 }
 
 
@@ -140,31 +105,19 @@ void getData(){
       case 0x80:    // Response
         return ;
       case 0x00:    // Request control
-      case 0x01: {    // Reset
-        uint8_t status[3] = { 0x15, (uint8_t)180, (uint8_t)0 } ;
-        fitnessMachineStatus.setValue(status, 3) ;
-        fitnessMachineStatus.notify() ;
-      }
+      case 0x01:     // Reset
       case 0x07:    // Start/stop
         break ;
-      case 0x05: {  // Target power level
+      case 0x05:    // Target power level
         target_power = (msg[2] << 8) + msg[1] ;
         Serial.print("Target Power: ") ;
         Serial.println(target_power) ; 
-        uint8_t status[3] = { 0x08, (uint8_t)msg[1], (uint8_t)msg[2] } ;
-        fitnessMachineStatus.setValue(status, 3) ;
-        fitnessMachineStatus.notify() ;
         break ;
-      }
-      case 0x11: {
+      case 0x11:
         grade = (msg[4] << 8) + msg[3] ;
         Serial.print("Grade: ") ;
         Serial.println(grade) ; 
-        uint8_t status[7] = { 0x12, (uint8_t)msg[1], (uint8_t)msg[2], (uint8_t)msg[2], (uint8_t)msg[3], (uint8_t)msg[4], (uint8_t)msg[5] } ;
-        fitnessMachineStatus.setValue(status, 7) ;
-        fitnessMachineStatus.notify() ;
         break ;
-      }
     }
     
     for (int i = 0 ; i < msg.length() ; i++){
